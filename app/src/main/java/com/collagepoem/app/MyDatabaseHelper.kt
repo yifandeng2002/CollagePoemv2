fef548024ca5f9f514bb943de78e119089bfcd7e
Rachelilens <1316887983@qq.com>
2023-01-27 22:55:31 +0800

    database

---
+++ app/src/main/java/com/collagepoem/app/MyDatabaseHelper.kt
@@ -0,0 +1,21 @@
+package com.collagepoem.app
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class MyDatabaseHelper(val context: Context, name: String, version: Int) :
+    SQLiteOpenHelper(context, name, null, version) {
+    private val createNotes = "create table Notes (" +
+            " id integer primary key autoincrement," +
+            "author text," +
+            "bookname text," +
+            "collecttime text,"+
+            "country text,"+
+            "image blob)"
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(createNotes)
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    }
+}
